c4a90351ca55c0bcf554b6982fd1b17354ce3eae
Tran Van Hoang <1714245ctk41@gmail.com>
2021-08-01 15:03:27 -0700

    push to git

---
+++ Employeemanagement_1/ViewModels/HomeDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Employeemanagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employeemanagement.ViewModels
+{
+    public class HomeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
---
+++ Employeemanagement_1/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Employeemanagement.Models;
+using Employeemanagement.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employeemanagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Remote(action: "IsEmailInUse", controller: "Account")]
+        //[ValidEmailDomain(allowedDomain: "pragimtech.com", ErrorMessage =" Email domain must be pragimtech.com")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm password")]
+        [Compare("Password",
+            ErrorMessage ="Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        public string City { get; set; }
+    }
+}
